1ebda4336f6feeaee8fcbd55588cac4fbb382680
sinlov <sinlovgmppt@gmail.com>
2018-02-08 21:06:49 +0800

    update show of check ENV

--- z-test-compileRelease.sh
+++ z-test-compileRelease.sh
@@ -16,21 +16,22 @@ android_build_task_last="compile${product_flavors}ReleaseJavaWithJavac"
 shell_run_path=$(cd `dirname $0`; pwd)
 
 checkFuncBack(){
-  if [ $? -eq 0 ]; then
-    echo "$1 success"
-  else
-    echo "$1 error stop build"
+  if [ $? -ne 0 ]; then
+    echo -e "\033[;31mRun [ $1 ] error exit code 1\033[0m"
     exit 1
+  # else
+  #   echo -e "\033[;30mRun [ $1 ] success\033[0m"
   fi
 }
 
 checkEnv(){
-    evn_checker=`which $1`
+  evn_checker=`which $1`
+  checkFuncBack "which $1"
   if [ ! -n "evn_checker" ]; then
-    echo "check event [ $1 ] error exit"
+    echo -e "\033[;31mCheck event [ $1 ] error exit\033[0m"
     exit 1
-  else
-    echo -e "Cli [ $1 ] event check success\n-> $1 at Path: ${evn_checker}"
+  # else
+  #   echo -e "\033[;32mCli [ $1 ] event check success\033[0m\n-> \033[;34m$1 at Path: ${evn_checker}\033[0m"
   fi
 }
 
